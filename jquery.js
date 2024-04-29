5e77bcc87fa37094a4f74e7b8d41bccc071ad8c1
Rahul patidar <141323154+patidarrahul0509@users.noreply.github.com>
2024-04-29 15:36:21 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,7 @@
+
+        document.querySelector("#bars").addEventListener("click",function(){
+            document.querySelector(".menu").style .top = "9.5%";
+        })
+        document.querySelector("#xmark").addEventListener("click",function(){
+            document.querySelector(".menu").style .top = "-100%";
+        })
